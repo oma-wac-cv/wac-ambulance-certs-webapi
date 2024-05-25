9819c68ce87f74d2c15ff4aee12aa719a5774236
Ondrej Martinka <ondrejmar23@gmail.com>
2024-05-25 21:41:27 +0200

    seed

--- internal/oma_wac_certs/impl_ambulance_staff_certifications.go
+++ internal/oma_wac_certs/impl_ambulance_staff_certifications.go
@@ -239,13 +239,59 @@ func (this *implAmbulanceStaffCertificationsAPI) SeedDatabase(ctx *gin.Context) 
 
   value, exists := ctx.Get("db_service_user")
   if !exists {
+    fmt.Println("err: db_service_user not found")
     return
   }
   
   db, ok := value.(db_service.DbService[User])
   if !ok {
+    fmt.Println("err: db_service_user not found")
     return
   }
+
+  value, exists = ctx.Get("db_service_cert")
+  if !exists {
+    fmt.Println("err: db_service_user not found")
+    return
+  }
+  
+  db_cert, ok := value.(db_service.DbService[Certification])
+  if !ok {
+    fmt.Println("err: db_service_user not found")
+    return
+  }
+
+  certifications := []Certification{
+    Certification{
+      Id: "7df518c4-bff0-4026-8f52-6290a39d59c2",
+      Name: "Basic Life Support (BLS)",
+      Description: "Training in life-saving techniques including CPR, AED usage, and relief of choking for healthcare providers.",
+      Authority: "American Red Cross (ARC)"},
+    Certification{
+      Id: "dcf01ae9-9b05-4276-aee7-2bd7cf75aea6",
+      Name: "Advanced Cardiovascular Life Support (ACLS)",
+      Description: "Advanced training in managing cardiac emergencies, including cardiac arrest, stroke, and acute coronary syndromes.",
+      Authority: "American Heart Association (AHA)"},
+    Certification{
+      Id: "32703805-4a2c-4e5c-91f0-a4ba25b81dc4",
+      Name: "Pediatric Advanced Life Support (PALS)",
+      Description: "Training in managing pediatric emergencies, including cardiac arrest, respiratory distress, and shock.",
+      Authority: "American Heart Association (AHA)"},
+    Certification{
+      Id: "d8d90583-3086-4fe0-b935-1a1445ba031b",
+      Name: "Emergency Medical Technician (EMT)",
+      Description: "Training in basic life support, patient assessment, and emergency care.",
+      Authority: "National Registry of Emergency Medical Technicians (NREMT)"},
+    Certification{
+      Id  : "1ddd60cd-befd-4927-b982-50301c7a059f",
+      Name: "Certified Medical Assistant (CMA)",
+      Description: "Certification for medical assistants demonstrating competency in clinical and administrative tasks in healthcare settings.",
+      Authority: "American Association of Medical Assistants (AAMA)"},
+    Certification{
+      Id: "d4647038-792e-4033-9893-18cdd3cb1b58",
+      Name: "Registered Nurse (RN)",
+      Description: "Training in patient care, medication administration, and treatment planning.",
+      Authority: "American Nurses Credentialing Center (ANCC)"}}
 
   ids := []string{
     "e6698483-7ef6-4432-acd9-baeb68830dae",
@@ -261,6 +307,25 @@ func (this *implAmbulanceStaffCertificationsAPI) SeedDatabase(ctx *gin.Context) 
     "Victoria M. Armstrong",
     "Miki M. Goff",
     "Dick K. Roberts",
+  }
+
+  cert := certifications[0];
+  for i := 0; i < len(certifications); i++ {
+    cert = certifications[i]
+    el, err := db_cert.FindDocument(ctx, cert.Id)
+
+    if el != nil {
+      fmt.Println("Database already seeded.")
+      ctx.JSON(http.StatusBadRequest, gin.H{"message": "Database already seeded"})
+      return
+    }
+
+    err = db_cert.CreateDocument(ctx, cert.Id, &cert)
+    if err != nil {
+      fmt.Println(err)
+      ctx.JSON(http.StatusBadRequest, gin.H{"message": "Database already seeded"})
+      return
+    }
   }
   
   // iterate over all names and create user for each
